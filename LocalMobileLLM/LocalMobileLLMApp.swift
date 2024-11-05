417686e63bbbd59a7a69b47c4ed8de7a0d7c3781
rencevio <berezhnoy.maxim@gmail.com>
2024-11-05 09:36:53 +0100

    Initial commit

---
+++ LocalMobileLLM/LocalMobileLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalMobileLLMApp.swift
+//  LocalMobileLLM
+//
+//  Created by Max on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalMobileLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
